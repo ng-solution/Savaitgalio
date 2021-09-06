da9640660dbd6f56842cf820ccbeedd76ac02e87
ng-solution <89936592+ng-solution@users.noreply.github.com>
2021-09-06 18:58:49 +0300

    Nauja uzduotis AZ
    Padaryta

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (char c='A'; c<='Z'; c++)
+            Console.WriteLine(c);
         }
     }
 }
